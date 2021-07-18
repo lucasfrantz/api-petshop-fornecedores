37608e5f4b2a4909983e578c3b245905dafb6b03
Lucas Frantz <maxkronus@gmail.com>
2021-07-18 20:57:01 -0300

    commit inicial

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('config')
+
+const roteador = require('./rotas/fornecedores')
+
+app.use(bodyParser.json())
+
+app.use('/api/fornecedores', roteador)
+
+
+app.listen(config.get('api.port'), () => {
+    console.log('A API esta funcionando')
+})
